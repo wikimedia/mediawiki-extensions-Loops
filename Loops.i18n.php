eac20fcfcb9ccbc7852fbbef5e382ef7fbfcfce2
Raimond Spekking <raymond@users.mediawiki.org>
2011-11-16 20:05:35 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- Loops.i18n.php
+++ Loops.i18n.php
@@ -19,11 +19,35 @@ $messages['en'] = array(
 	'loops_max' => 'Maximum number of loops have been performed'
 );
 
-/** German
+/** German (Deutsch)
  * @author Daniel Werner
  */
 $messages['de'] = array(
 	'loops-desc' => 'Parser-Funktionen zur Schleifen-Ausführung',
-	'loops_max' => 'Die maximal erlaubte Anzahl an Schleifen wurde ausgeführt'
+	'loops_max' => 'Die maximal erlaubte Anzahl an Schleifen wurde ausgeführt',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'loops-desc' => 'Funcións analíticas para levar a cabo bucles',
+	'loops_max' => 'Levouse a cabo o número máximo de bucles',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'loops-desc' => 'Парсерски функции за вршење на повторувања (јамки)',
+	'loops_max' => 'Извршен е максималниот број на повторувања',
+);
+
+/** Malay (Bahasa Melayu)
+ * @author Anakmalaysia
+ */
+$messages['ms'] = array(
+	'loops-desc' => 'Fungsi parser untuk melakukan gelung',
+	'loops_max' => 'Bilangan gelung maksimum telah dilakukan',
 );
 
